730befb96798a7d408c0fa133b35a963a759b547
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-12-17 01:00:28 +0200

    alphabetical sorting for contacts provided

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,12 +1,27 @@
 import s from 'components/ContactsList/ContactsList.module.css';
 
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 
 function ContactsList({ contacts }) {
+  const [sortedContacts, setsortedContacts] = useState([]);
+
+  useEffect(() => {
+    const makeSortedContacts = () => {
+      return [...contacts].sort((x, y) => {
+        let a = x.name.toUpperCase(),
+          b = y.name.toUpperCase();
+        return a === b ? 0 : a > b ? 1 : -1;
+      });
+    };
+
+    setsortedContacts(makeSortedContacts());
+  }, [contacts]);
+
   return (
     <ul className={s.list}>
-      {contacts.map(el => {
+      {sortedContacts.map(el => {
         return (
           <ContactItem
             key={el.id}
